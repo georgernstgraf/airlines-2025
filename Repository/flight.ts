95cb00d365fac3d0bde5e9fded2e1ede9cb262e7
AdnanFRR <lju22097@spengergasse.at>
2025-12-17 14:46:44 +0100

    Sollte gehen

--- Repository/flight.ts
+++ Repository/flight.ts
@@ -1,12 +1,16 @@
 import { Prisma } from "../prisma/client/browser.ts";
 import { prisma } from "./db.ts"
 
+// CREATE
+export async function create(data: Prisma.FlightCreateArgs["data"]) {
+    return await prisma.flight.create({ data });
+}
+
+// READ
 export async function count() {
     return await prisma.flight.count();
 }
-export async function create(data:Prisma.FlightCreateArgs["data"]) {
-    return await prisma.flight.create({data});
-}
+
 export async function findMany() {
     return await prisma.flight.findMany();
 }
@@ -34,6 +38,33 @@ export async function findById(id: string) {
     });
 }
 
+export async function findByFlightNumber(flightNumber: string) {
+    return await prisma.flight.findFirst({
+        where: { flightNumber },
+        include: {
+            passengers: true,
+            plane: true,
+            origin: true,
+            destination: true,
+        }
+    });
+}
+
+// UPDATE
+export async function update(id: string, data: Prisma.FlightUpdateInput) {
+    return await prisma.flight.update({
+        where: { id },
+        data
+    });
+}
+
 export async function assignPassenger(id: string, data: Prisma.FlightUpdateInput) {
     return await prisma.flight.update({ where: { id }, data });
+}
+
+// DELETE
+export async function delete_(id: string) {
+    return await prisma.flight.delete({
+        where: { id }
+    });
 }
--- Repository/plane.ts
+++ Repository/plane.ts
@@ -1,12 +1,57 @@
 import { Prisma } from "../prisma/client/browser.ts";
 import { prisma } from "./db.ts"
 
+// CREATE
+export async function create(data: Prisma.PlaneCreateArgs["data"]) {
+    return await prisma.plane.create({ data });
+}
+
+// READ
 export async function count() {
     return await prisma.plane.count();
 }
-export async function create(data:Prisma.PlaneCreateArgs["data"]) {
-    return await prisma.plane.create({data});
-}
+
 export async function getAll() {
     return await prisma.plane.findMany();
+}
+
+export async function getAllWithFlights() {
+    return await prisma.plane.findMany({
+        include: {
+            flights: true,
+        }
+    });
+}
+
+export async function getById(id: string) {
+    return await prisma.plane.findUnique({
+        where: { id },
+        include: {
+            flights: true,
+        }
+    });
+}
+
+export async function getByModel(model: string) {
+    return await prisma.plane.findMany({
+        where: { model },
+        include: {
+            flights: true,
+        }
+    });
+}
+
+// UPDATE
+export async function update(id: string, data: Prisma.PlaneUpdateInput) {
+    return await prisma.plane.update({
+        where: { id },
+        data
+    });
+}
+
+// DELETE
+export async function delete_(id: string) {
+    return await prisma.plane.delete({
+        where: { id }
+    });
 }
